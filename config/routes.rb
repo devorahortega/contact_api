11dfd396478f996e3f007ce1d160f3aaac26d5a5
Devorah Ortega <devorahb.ortega@gmail.com>
2021-10-12 15:16:38 -0400

    Creates seed file

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  get "/one_contact", controller: "contacts", action: "one_contact"
+  get "/one_contact" => "contacts#one_contact"
+
+  get "/all_contacts" => "contacts#all_contacts"
 end
