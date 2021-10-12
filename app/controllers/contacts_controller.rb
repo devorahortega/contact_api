bde47d74eebb6b0eb05d3f79010bedcbf663d6fa
Devorah Ortega <devorahb.ortega@gmail.com>
2021-10-12 14:41:14 -0400

    Adds route

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,11 +1,12 @@
 class ContactsController < ApplicationController
-  def one_contact
-    contact = Contact.first
-    render json: contact.as_json
-  end
+end
 
-  def all_contacts
-    contacts = Contact.all_contacts
-    render json: contacts.as_json
-  end
+def one_contact
+  contact = Contact.first
+  render json: contact.as_json
+end
+
+def all_contacts
+  contacts = Contact.all_contacts
+  render json: contacts.as_json
 end
