f411caf4daf361698c2bb1c471b9b626d3331dd1
Devorah Ortega <devorahb.ortega@gmail.com>
2022-01-04 19:05:26 -0500

    Initial commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,5 @@
 Contact.create(first_name: "Jane", last_name: "Jones", email: "janejones@gmail.com", phone_number: 12315512)
+
+#Contact.create(first_name: "Jane", last_name: "Jones", email: "janejones@gmail.com", phone_number: 12315512)
+#Contact.create(first_name: "Jane", last_name: "Jones", email: "janejones@gmail.com", phone_number: 12315512)
+#Contact.create(first_name: "Jane", last_name: "Jones", email: "janejones@gmail.com", phone_number: 12315512)
